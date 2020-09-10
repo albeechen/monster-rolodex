f85439713d350eafe944d507c73b4d2ae591decd
albeec <albeec0902@gmail.com>
2020-09-10 09:03:17 -0700

    Updates

---
+++ precache-manifest.606368bb1fcf9a42a980da001973c1fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab6858aa87c2f49b2a6a219b02ca231",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f418d325ffa6884a9b09",
+    "url": "/monster-rolodex/static/css/main.2e150a43.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f418d325ffa6884a9b09",
+    "url": "/monster-rolodex/static/js/main.5dd531a3.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
